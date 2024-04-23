d6079c95fd6296eb41e3279562c165e1a6c88154
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-23 13:21:42 -0300

    chore: initial setup

---
+++ src/config/env.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+
+import z from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['production', 'testing', 'development'])
+    .default('development'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.log('Invalid environment variables', _env.error.flatten())
+
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
