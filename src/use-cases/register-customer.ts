190abe667bcbbbe20f6ec02b78024c74971a7ea0
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-23 14:37:28 -0300

    feat(register-customer.ts): add RegisterCustomerUseCase class to handle customer registration process

---
+++ src/use-cases/register-customer.ts
@@ -0,0 +1,49 @@
+import { Customer } from '@/entities/customer'
+import { HashGenerator } from '@/cryptography/hash-generator'
+import { CustomersRepository } from '@/repositories/customers-repository'
+
+import { CustomerAlreadyExistsError } from './errors/customer-already-exists-error'
+
+interface RegisterCustomerUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+interface RegisterCustomerUseCaseResponse {
+  customer: Customer
+}
+
+export class RegisterCustomerUseCase {
+  constructor(
+    private readonly customersRepository: CustomersRepository,
+    private readonly hashGenerator: HashGenerator,
+  ) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterCustomerUseCaseRequest): Promise<RegisterCustomerUseCaseResponse> {
+    const customerWithSameEmail =
+      await this.customersRepository.findByEmail(email)
+
+    if (customerWithSameEmail) {
+      throw new CustomerAlreadyExistsError(email)
+    }
+
+    const hashedPassword = await this.hashGenerator.hash(password)
+
+    const customer = Customer.create({
+      name,
+      email,
+      password: hashedPassword,
+    })
+
+    await this.customersRepository.create(customer)
+
+    return {
+      customer,
+    }
+  }
+}
