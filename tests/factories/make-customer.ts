7e8c042ec4fd858461926fea2a818857ecb36088
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-23 14:36:51 -0300

    feat(tests): add factory function makeCustomer to generate mock Customer objects for testing purposes

---
+++ tests/factories/make-customer.ts
@@ -0,0 +1,15 @@
+import { faker } from '@faker-js/faker'
+
+import { Customer } from '@/entities/customer'
+
+export function makeCustomer(override: Partial<Customer> = {}): Customer {
+  return Customer.create(
+    {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      ...override,
+    },
+    override.id,
+  )
+}
