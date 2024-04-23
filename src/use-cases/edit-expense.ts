530a2063bb265cd8959580bdfb97c51ad53119ee
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-23 16:54:33 -0300

    feat(edit-expense.ts): add EditExpenseUseCase class to handle editing expenses with request and response interfaces

---
+++ src/use-cases/edit-expense.ts
@@ -0,0 +1,47 @@
+import { Expense } from '@/entities/expense'
+import { ExpensesRepository } from '@/repositories/expenses-repository'
+
+import { ExpenseNotFoundError } from './errors/expense-not-found-error'
+
+interface EditExpenseUseCaseRequest {
+  customerId: string
+  expenseId: string
+  description: string
+  amount: number
+  date: Date
+}
+
+interface EditExpenseUseCaseResponse {
+  expense: Expense
+}
+
+export class EditExpenseUseCase {
+  constructor(private readonly expensesRepository: ExpensesRepository) {}
+
+  async execute({
+    customerId,
+    expenseId,
+    description,
+    amount,
+    date,
+  }: EditExpenseUseCaseRequest): Promise<EditExpenseUseCaseResponse> {
+    const expense = await this.expensesRepository.findByCustomerId(
+      customerId,
+      expenseId,
+    )
+
+    if (!expense) {
+      throw new ExpenseNotFoundError()
+    }
+
+    expense.description = description
+    expense.amount = amount
+    expense.date = date
+
+    await this.expensesRepository.save(expense)
+
+    return {
+      expense,
+    }
+  }
+}
