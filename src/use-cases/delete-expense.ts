281283d2a59140d063522b5abfcd3ec612871425
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-23 16:54:54 -0300

    feat(delete-expense.ts): add DeleteExpenseUseCase to handle deleting expenses from the repository based on customer and expense ID

---
+++ src/use-cases/delete-expense.ts
@@ -0,0 +1,28 @@
+import { ExpensesRepository } from '@/repositories/expenses-repository'
+
+import { ExpenseNotFoundError } from './errors/expense-not-found-error'
+
+interface DeleteExpenseUseCaseRequest {
+  customerId: string
+  expenseId: string
+}
+
+export class DeleteExpenseUseCase {
+  constructor(private readonly expensesRepository: ExpensesRepository) {}
+
+  async execute({
+    customerId,
+    expenseId,
+  }: DeleteExpenseUseCaseRequest): Promise<void> {
+    const expense = await this.expensesRepository.findByCustomerId(
+      customerId,
+      expenseId,
+    )
+
+    if (!expense) {
+      throw new ExpenseNotFoundError()
+    }
+
+    await this.expensesRepository.delete(expense)
+  }
+}
