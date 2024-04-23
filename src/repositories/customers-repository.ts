1935d150384637fc1dad1bfff1d856b1c938ef67
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-23 16:50:36 -0300

    feat(customers-repository.ts): add findById method to CustomersRepository interface to retrieve a customer by id

--- src/repositories/customers-repository.ts
+++ src/repositories/customers-repository.ts
@@ -1,6 +1,7 @@
 import { Customer } from '@/entities/customer'
 
 export interface CustomersRepository {
+  findById(id: string): Promise<Customer | null>
   findByEmail(email: string): Promise<Customer | null>
   create(customer: Customer): Promise<void>
 }
