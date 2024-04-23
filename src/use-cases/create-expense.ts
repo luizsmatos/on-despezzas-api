b5e1928d31715370e2c886fe49ab63f88d556e60
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-23 16:53:06 -0300

    feat(create-expense.ts): add CreateExpenseUseCase class to handle creation of expenses

---
+++ src/use-cases/create-expense.ts
@@ -0,0 +1,37 @@
+import { Expense } from '@/entities/expense'
+import { ExpensesRepository } from '@/repositories/expenses-repository'
+
+interface CreateExpenseUseCaseRequest {
+  customerId: string
+  description: string
+  amount: number
+  date: Date
+}
+
+interface CreateExpenseUseCaseResponse {
+  expense: Expense
+}
+
+export class CreateExpenseUseCase {
+  constructor(private readonly expensesRepository: ExpensesRepository) {}
+
+  async execute({
+    customerId,
+    description,
+    amount,
+    date,
+  }: CreateExpenseUseCaseRequest): Promise<CreateExpenseUseCaseResponse> {
+    const expense = Expense.create({
+      customerId,
+      description,
+      amount,
+      date,
+    })
+
+    await this.expensesRepository.create(expense)
+
+    return {
+      expense,
+    }
+  }
+}
