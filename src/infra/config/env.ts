7f1c2708a84a69a9ddbf4a51f6fdc059a19cb69d
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-24 18:25:45 -0300

    feat(env.ts): add DATABASE_URL validation using z.string().url() to ensure correct URL format is provided

--- src/infra/config/env.ts
+++ src/infra/config/env.ts
@@ -7,6 +7,7 @@ const envSchema = z.object({
     .enum(['production', 'testing', 'development'])
     .default('development'),
   PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string().url(),
   JWT_SECRET: z.string(),
 })
 
