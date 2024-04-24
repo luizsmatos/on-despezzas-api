cf0e0b31f6ad5dbc7ab223c0ccefc311e5799a41
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-24 16:01:06 -0300

    refactor(prisma-expense-mapper.ts): update amount field mapping to convert to Decimal type for consistency and accuracy

--- src/infra/database/prisma/mappers/prisma-expense-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-expense-mapper.ts
@@ -1,4 +1,5 @@
 import { Expense as PrismaExpense, Prisma } from '@prisma/client'
+import { Decimal } from '@prisma/client/runtime/library'
 import { Expense } from '@/domain/entities/expense'
 
 export class PrismaExpenseMapper {
@@ -6,7 +7,7 @@ export class PrismaExpenseMapper {
     return {
       id: expense.id,
       description: expense.description,
-      amount: expense.amount,
+      amount: new Decimal(expense.amount).toNumber(),
       date: expense.date,
       customerId: expense.customerId,
       createdAt: expense.createdAt,
@@ -18,7 +19,7 @@ export class PrismaExpenseMapper {
     return Expense.create(
       {
         description: raw.description,
-        amount: raw.amount,
+        amount: new Decimal(raw.amount).toNumber(),
         date: raw.date,
         customerId: raw.customerId,
         createdAt: raw.createdAt,
