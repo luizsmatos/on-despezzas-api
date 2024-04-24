fadb6050c09406de51c272e1e2840603f5fa4b5b
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-24 18:26:57 -0300

    feat(app.ts): add support for express-async-errors package to handle asynchronous errors more effectively

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,11 +1,16 @@
+import 'express-async-errors'
 import express from 'express'
 
+import { errorHandlerMiddleware } from './http/middlewares/error-handler-middleware'
 import { customersRouter } from './http/routes/customers-routes'
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(customersRouter)
+
+app.use(errorHandlerMiddleware)
 
 export { app }
