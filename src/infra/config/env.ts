33cda53a757117ba855ebc16606233415be612e2
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-24 15:41:09 -0300

    feat(env.ts): add JWT_SECRET environment variable to store JWT secret key for authentication purposes

--- src/infra/config/env.ts
+++ src/infra/config/env.ts
@@ -7,6 +7,7 @@ const envSchema = z.object({
     .enum(['production', 'testing', 'development'])
     .default('development'),
   PORT: z.coerce.number().default(3333),
+  JWT_SECRET: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
