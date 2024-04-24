1fb355402f241623d0f155b532c9708d748f4581
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-24 20:59:03 -0300

    feat(app.ts): add expensesRoutes to handle expenses-related routes in the application

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -2,14 +2,16 @@ import 'express-async-errors'
 import express from 'express'
 
 import { errorHandlerMiddleware } from './http/middlewares/error-handler-middleware'
-import { customersRouter } from './http/routes/customers-routes'
+import { customersRoutes } from './http/routes/customers-routes'
+import { expensesRoutes } from './http/routes/expenses-routes'
 
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use(customersRouter)
+app.use(customersRoutes)
+app.use(expensesRoutes)
 
 app.use(errorHandlerMiddleware)
 
