2e8c1fdb7abf584189c50d76cdc9a28299f30cd4
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-24 18:33:49 -0300

    feat(customers-routes.ts): add route for customer login to authenticateCustomerController

--- src/infra/http/routes/customers-routes.ts
+++ src/infra/http/routes/customers-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 
+import { authenticateCustomerController } from '../controllers/authenticate-customer-controller'
 import { registerCustomerController } from '../controllers/register-customer-controller'
 
 const customersRouter = Router()
 
 customersRouter.post('/customers', registerCustomerController)
+customersRouter.post('/customers/login', authenticateCustomerController)
 
 export { customersRouter }
