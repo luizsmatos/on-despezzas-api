a937f27f80d2da359df32cc79f9ca0bfda8c37e3
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-25 13:17:13 -0300

    refactor(ethereal-mail-provider.ts): refactor EtherealMailProvider to initialize transporter in a separate async method

--- src/infra/providers/mail/ethereal/ethereal-mail-provider.ts
+++ src/infra/providers/mail/ethereal/ethereal-mail-provider.ts
@@ -5,28 +5,25 @@ import { MailProvider, SendParams } from '../mail-provider'
 export class EtherealMailProvider implements MailProvider {
   private transporter!: nodemailer.Transporter
 
-  constructor() {
-    nodemailer
-      .createTestAccount()
-      .then((account) => {
-        this.transporter = nodemailer.createTransport({
-          host: account.smtp.host,
-          port: account.smtp.port,
-          secure: account.smtp.secure,
-          auth: {
-            user: account.user,
-            pass: account.pass,
-          },
-        })
-      })
-      .catch((error) => {
-        console.error(error)
+  private async init(): Promise<void> {
+    const testAccount = await nodemailer.createTestAccount()
 
-        throw new Error('Error creating EtherealMailProvider')
-      })
+    this.transporter = nodemailer.createTransport({
+      host: testAccount.smtp.host,
+      port: testAccount.smtp.port,
+      secure: testAccount.smtp.secure,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass,
+      },
+    })
   }
 
   async send({ to, subject, body }: SendParams): Promise<void> {
+    if (!this.transporter) {
+      await this.init()
+    }
+
     const message = await this.transporter.sendMail({
       from: 'noreply@on-despezzas.com',
       to,
