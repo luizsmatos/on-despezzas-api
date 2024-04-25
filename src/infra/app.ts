ab4475bcae0386fb75574d30bebb111440e1fbf7
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-25 16:42:05 -0300

    feat(app.ts): add swagger documentation middleware to generate API documentation

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -2,6 +2,7 @@ import 'express-async-errors'
 import express from 'express'
 
 import { errorHandlerMiddleware } from './http/middlewares/error-handler-middleware'
+import { swaggerMiddleware } from './http/middlewares/swagger-middleware'
 import { customersRoutes } from './http/routes/customers-routes'
 import { expensesRoutes } from './http/routes/expenses-routes'
 
@@ -9,6 +10,12 @@ const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+swaggerMiddleware(app)
+
+app.all('/', (_request, response) => {
+  response.redirect('/docs')
+})
 
 app.use(customersRoutes)
 app.use(expensesRoutes)
