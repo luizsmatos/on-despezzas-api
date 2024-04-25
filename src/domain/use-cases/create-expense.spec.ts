85c41bd8bd02c2d4f26fe0c6f68ea896e152653e
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-25 12:33:45 -0300

    feat(create-expense.spec.ts): add support for sending email notifications when a new expense is created to enhance user experience

--- src/domain/use-cases/create-expense.spec.ts
+++ src/domain/use-cases/create-expense.spec.ts
@@ -1,16 +1,22 @@
 import { makeCustomer } from 'tests/factories/make-customer'
+import { InMemoryMailProvider } from 'tests/providers/in-memory-mail-provider'
 import { InMemoryExpensesRepository } from 'tests/repositories/in-memory-expenses-repository'
 
 import { CreateExpenseUseCase } from './create-expense'
 
 let sut: CreateExpenseUseCase
 let inMemoryExpensesRepository: InMemoryExpensesRepository
+let inMemoryMailProvider: InMemoryMailProvider
 
 describe('Create Expense UseCase', () => {
   beforeEach(() => {
     inMemoryExpensesRepository = new InMemoryExpensesRepository()
+    inMemoryMailProvider = new InMemoryMailProvider()
 
-    sut = new CreateExpenseUseCase(inMemoryExpensesRepository)
+    sut = new CreateExpenseUseCase(
+      inMemoryExpensesRepository,
+      inMemoryMailProvider,
+    )
   })
 
   it('should be able create a new expense', async () => {
@@ -18,6 +24,7 @@ describe('Create Expense UseCase', () => {
 
     const result = await sut.execute({
       customerId: customer.id,
+      email: customer.email,
       description: 'New expense',
       amount: 100,
       date: new Date(),
@@ -29,5 +36,6 @@ describe('Create Expense UseCase', () => {
       }),
     })
     expect(inMemoryExpensesRepository.items).toHaveLength(1)
+    expect(inMemoryMailProvider.items).toHaveLength(1)
   })
 })
