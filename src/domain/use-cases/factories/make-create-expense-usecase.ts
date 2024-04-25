56e1b1a762465b909ae0d9d133594fa8b110f462
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-25 13:57:43 -0300

    refactor(make-create-expense-usecase.ts): update makeCreateExpenseUseCase factory to conditionally use InMemoryMailProvider for testing environment

--- src/domain/use-cases/factories/make-create-expense-usecase.ts
+++ src/domain/use-cases/factories/make-create-expense-usecase.ts
@@ -2,10 +2,15 @@ import { PrismaExpensesRepository } from '@/infra/database/prisma/repositories/p
 import { EtherealMailProvider } from '@/infra/providers/mail/ethereal/ethereal-mail-provider'
 
 import { CreateExpenseUseCase } from '../create-expense'
+import { env } from '@/infra/config/env'
+import { InMemoryMailProvider } from 'tests/providers/in-memory-mail-provider'
 
 export function makeCreateExpenseUseCase(): CreateExpenseUseCase {
   const expensesRepository = new PrismaExpensesRepository()
-  const mailProvider = new EtherealMailProvider()
+  const mailProvider =
+    env.NODE_ENV !== 'testing'
+      ? new EtherealMailProvider()
+      : new InMemoryMailProvider()
 
   const useCase = new CreateExpenseUseCase(expensesRepository, mailProvider)
 
