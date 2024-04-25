fc7fb9cd080995f5629cbff4209a19aa395aaba1
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-25 12:33:33 -0300

    refactor(make-create-expense-usecase.ts): add EtherealMailProvider to CreateExpenseUseCase constructor for sending emails in expense creation process

--- src/domain/use-cases/factories/make-create-expense-usecase.ts
+++ src/domain/use-cases/factories/make-create-expense-usecase.ts
@@ -1,11 +1,13 @@
 import { PrismaExpensesRepository } from '@/infra/database/prisma/repositories/prisma-expenses-repository'
+import { EtherealMailProvider } from '@/infra/providers/mail/ethereal/ethereal-mail-provider'
 
 import { CreateExpenseUseCase } from '../create-expense'
 
 export function makeCreateExpenseUseCase(): CreateExpenseUseCase {
   const expensesRepository = new PrismaExpensesRepository()
+  const mailProvider = new EtherealMailProvider()
 
-  const useCase = new CreateExpenseUseCase(expensesRepository)
+  const useCase = new CreateExpenseUseCase(expensesRepository, mailProvider)
 
   return useCase
 }
