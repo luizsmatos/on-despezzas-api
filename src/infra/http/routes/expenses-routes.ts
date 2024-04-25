4497fff59d5f7ff837a704f4fed51c40ca8ed823
Luiz Gustavo <gusstavopdm@gmail.com>
2024-04-24 22:51:01 -0300

    feat(expenses-routes.ts): add routes for listing, getting, editing, and deleting expenses to enhance CRUD functionality

--- src/infra/http/routes/expenses-routes.ts
+++ src/infra/http/routes/expenses-routes.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express'
 
 import { createExpenseController } from '../controllers/create-expense-controller'
+import { listExpensesController } from '../controllers/list-expenses-controller'
+import { getExpenseController } from '../controllers/get-expense-controller'
+import { editExpenseController } from '../controllers/edit-expense-controller'
+import { deleteExpenseController } from '../controllers/delete-expense-controller'
 import { verifyJwtMiddleware } from '../middlewares/verify-jwt-middleware'
 
 const expensesRoutes = Router()
 
 expensesRoutes.use(verifyJwtMiddleware)
 expensesRoutes.post('/expenses', createExpenseController)
+expensesRoutes.get('/expenses', listExpensesController)
+expensesRoutes.get('/expenses/:id', getExpenseController)
+expensesRoutes.put('/expenses/:id', editExpenseController)
+expensesRoutes.delete('/expenses/:id', deleteExpenseController)
 
 export { expensesRoutes }
